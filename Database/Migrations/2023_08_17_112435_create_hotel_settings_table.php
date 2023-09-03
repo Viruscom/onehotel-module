38890249888d449b89988779bba52575c31dc86a
h.chobanov <hristiqn.chobanov@gmail.com>
2023-09-03 14:35:39 +0300

    sdfsf

--- Database/Migrations/2023_08_17_112435_create_hotel_settings_table.php
+++ Database/Migrations/2023_08_17_112435_create_hotel_settings_table.php
@@ -15,7 +15,7 @@
         {
             Schema::create('hotel_settings', function (Blueprint $table) {
                 $table->id();
-                $table->text('default_reservation_system')->default('inquiry');
+                $table->text('default_reservation_system')->nullable()->default('inquiry');
                 $table->text('clientric_key')->nullable();
                 $table->text('clock_key')->nullable();
                 $table->text('travelline_key')->nullable();
