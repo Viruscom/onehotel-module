4e285cf235caf0f1bea1b4c7f05cb610fdbe686c
h.chobanov <hristiqn.chobanov@gmail.com>
2023-09-06 17:41:39 +0300

    sdfsdf

---
+++ Resources/views/admin/rooms_occupancy/breadcrumbs.blade.php
@@ -0,0 +1,11 @@
+<div class="breadcrumbs">
+    <ul>
+        <li>
+            <a href="{{ route('admin.index') }}"><i class="fa fa-home"></i></a>
+            <i class="fa fa-angle-right"></i>
+        </li>
+        <li>
+            <a href="{{ route('admin.pages.index') }}" class="text-black">@lang('onehotel::admin.rooms_occupancy.index')</a>
+        </li>
+    </ul>
+</div>
