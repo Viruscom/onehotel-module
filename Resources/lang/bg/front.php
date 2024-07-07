4f19fc0bb8917d9e6409f2518c80c38a4cc94a32
h.chobanov <hristiqn.chobanov@gmail.com>
2024-07-07 10:29:26 +0300

    lang array key added

--- Resources/lang/bg/front.php
+++ Resources/lang/bg/front.php
@@ -1,8 +1,9 @@
 <?php
 
-return array (
-  'book_status_available' => 'Available',
-  'book_status_booked_or_closed' => 'Booked or closed',
-  'book_status_booked_am' => 'Booked a.m.',
-  'book_status_booked_pm' => 'Booked p.m.',
-);
+    return array(
+        'book_status_available'        => 'Available',
+        'book_status_booked_or_closed' => 'Booked or closed',
+        'book_status_booked_am'        => 'Booked a.m.',
+        'book_status_booked_pm'        => 'Booked p.m.',
+        'book_now_header'              => 'Book now',
+    );
